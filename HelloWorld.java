5ffa1dc1e105b19e4dd552debd272c0b44e24ca7
Yabadaba <allenbao01@gmail.com>
2021-11-30 21:29:04 -0800

    bruh

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/* 
+ * Author: Allen Bao
+ * Date: 9/27
+ * File: HelloWorld.java
+  
+ * prints "Hello World!" to the screen
+ */
+public class HelloWorld {
+   public static void main (String[] args) {
+      System.out.println("Hello World!");
+   }
+}
